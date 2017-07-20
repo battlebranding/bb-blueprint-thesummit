d8b207c6531b2ae1ca5fda2dd0e7cb111e82dd06
Marcus <marcus@marcusbattle.com>
2017-07-20 06:44:06 +0000

    Added the scaffold for the homepage elements

--- home.php
+++ home.php
@@ -1,5 +1,128 @@
 <?php get_header(); ?>
 
-Welcome to The Summit.
+<header>
+	<div class="container">
+		<h1>The Summit</h1>
+		<nav class="nav is-horizontal">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Small Groups</a></li>
+				<li><a href="#">Events</a></li>
+				<li><a href="#">Faith</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+<section>
+	<div class="container">
+		<h2>Small Groups build community</h2>
+		<p>This is the place and time where we list out the available small groups for a user to view.</p>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<div class="columns">
+			<div class="column is-fullwidth-on-mobile">
+				<h3>Featured Service Two</h3>
+				<p>Here are 2-4 sentences about featured service #1.</p>
+				<a class="button is-outlined" href="#">Learn More</a>
+			</div>
+			<div class="column is-fullwidth-on-mobile">
+				<h3>Summit Arts</h3>
+				<p>Here are 2-4 sentences about featured service #2.</p>
+				<a class="button is-outlined" href="#">Learn More</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<h2>Upcoming Events</h2>
+		<p>This section will contain an upcoming event.</p>
+	</div>
+</section>
+
+<section>
+	<div class="container has-text-centered">
+		<p>This will contain a testimonial.</p>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<p>Here are 2-3 sentences about The Summit, because it's important for it to be here.</p>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<p>Here are 2-3 sentences about New Jerusalem Cathedral.</p>
+	</div>
+</section>
+
+<section>
+	<div class="container has-text-centered">
+		<p>This will contain a testimonial.</p>
+	</div>
+</section>
+
+
+
+
+
+<section>
+	<div class="container">
+		<h2>Blog Posts // Build Your Faith</h2>
+		<div class="columns">
+			<article class="column">
+				<h3>Blog Post One</h3>
+				<p>This will contain an excerpt for the blog post.</p>
+				<a class="button is-outlined">Read More</a>
+			</article>
+			<article class="column">
+				<h3>Blog Post Two</h3>
+				<p>This will contain an excerpt for the blog post.</p>
+				<a class="button is-outlined">Read More</a>
+			</article>
+			<article class="column">
+				<h3>Blog Post Two</h3>
+				<p>This will contain an excerpt for the blog post.</p>
+				<a class="button is-outlined">Read More</a>
+			</article>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<p>Here's some information to introduce the alumni team of The Summit.</p>
+		<div class="columns">
+			<div class="column is-fullwidth-on-mobile">
+				<h3>Marcus Battle</h3>
+				<p></p>
+			</div>
+			<div class="column is-fullwidth-on-mobile">
+				<h3>Tammy Battle</h3>
+			</div>
+			<div class="column is-fullwidth-on-mobile">
+				<h3>Michael Clinton</h3>
+			</div>
+			<div class="column is-fullwidth-on-mobile">
+				<h3>Rosemary Williams</h3>
+			</div>
+		</div>
+	</div>
+</section>
+
+<footer>
+	<div class="container">
+		<h2>The Summit</h2>
+	</div>
+</footer>
 
 <?php get_footer(); ?>
